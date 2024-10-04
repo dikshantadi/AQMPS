3339d6292d8f26a6a2a552fa59808c8f20fb6437
Dikshyant Adhikari <adhikarybad@gmail.com>
2024-10-04 17:26:03 +0545

    frontend start

---
+++ aqmps/lib/Startpg.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Startpg extends StatefulWidget {
+  const Startpg({super.key});
+
+  @override
+  State<Startpg> createState() => _StartpgState();
+}
+
+class _StartpgState extends State<Startpg> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
