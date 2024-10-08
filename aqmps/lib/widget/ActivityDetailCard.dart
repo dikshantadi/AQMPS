c525e4d8151316c954f70c0499c55834a4d0a821
Dikshyant Adhikari <adhikarybad@gmail.com>
2024-10-08 15:04:17 +0545

    aa

--- aqmps/lib/widget/ActivityDetailCard.dart
+++ aqmps/lib/widget/ActivityDetailCard.dart
@@ -3,33 +3,58 @@ import 'package:aqmps/widget/Custom_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class Activitydetailcard extends StatelessWidget {
-  const Activitydetailcard({super.key});
+  final String sensorName;
+
+  const Activitydetailcard({super.key, required this.sensorName});
 
   @override
   Widget build(BuildContext context) {
     final pollutiondetail = PollutionDetail();
-    return GridView.builder(
-      itemCount: pollutiondetail.PollutionData.length,
-      shrinkWrap: true,
-      physics: const ScrollPhysics(),
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4, crossAxisSpacing: 15, mainAxisSpacing: 12),
-      itemBuilder: (context, index) => CustomCard(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              pollutiondetail.PollutionData[index].icon,
-              width: 30,
-              height: 30,
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            sensorName,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(pollutiondetail.PollutionData[index].title),
-            )
-          ],
-        ),
+          ),
+          const SizedBox(height: 8),
+          GridView.builder(
+            itemCount: pollutiondetail.PollutionData.length,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              crossAxisSpacing: 15,
+              mainAxisSpacing: 12,
+            ),
+            itemBuilder: (context, index) => CustomCard(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    pollutiondetail.PollutionData[index].icon,
+                    width: 30,
+                    height: 30,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(pollutiondetail.PollutionData[index].title),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(pollutiondetail.PollutionData[index].value),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- aqmps/lib/widget/Dashboard.dart
+++ aqmps/lib/widget/Dashboard.dart
@@ -7,15 +7,21 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        SizedBox(
-          height: 18,
-        ),
-        Headerwidget(),
-        const SizedBox(height: 18),
-        Activitydetailcard(),
-      ],
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 18,
+          ),
+          Headerwidget(),
+          const SizedBox(height: 18),
+          Activitydetailcard(sensorName: "Sensor 1"),
+          const SizedBox(height: 18),
+          const Activitydetailcard(sensorName: "Sensor 2"),
+          const SizedBox(height: 18),
+          Activitydetailcard(sensorName: "Sensor 3"),
+        ],
+      ),
     );
   }
 }
